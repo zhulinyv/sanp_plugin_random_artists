96ab253bf3f1bf482f5ee015cf964461e21bda18
zhulinyv <zhulinyv2005@outlook.com>
2024-08-04 16:46:49 +0800

    :zap: 同步上游更改

--- utils.py
+++ utils.py
@@ -9,23 +9,12 @@ from utils.utils import (
     file_path2list,
     format_str,
     generate_image,
+    generate_random_str,
     read_json,
     read_txt,
     save_image,
     sleep_for_cool,
 )
-
-
-def generate_random_str(randomlength):
-    """
-    生成一个指定长度的随机字符串
-    """
-    random_str = ""
-    base_str = "ABCDEFGHIGKLMNOPQRSTUVWXYZabcdefghigklmnopqrstuvwxyz0123456789"
-    length = len(base_str) - 1
-    for i in range(randomlength):
-        random_str += base_str[random.randint(0, length)]
-    return random_str
 
 
 def random_artists(
